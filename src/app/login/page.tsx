a0e75b06f546b9d59dfa5a56701794753a8c3bf7
oz841119 <oz841119@gmail.com>
2023-07-01 01:34:48 +0800

    none

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,41 @@
 "use client"
 import { Button, TextField } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const userJSON = window.localStorage.getItem('user')
+    if(userJSON) {
+      const accountInp = document.getElementById('login-account') as HTMLInputElement
+      const passwordInp = document.getElementById('login-password') as HTMLInputElement
+      const user = JSON.parse(userJSON)
+      accountInp.value = user.account
+      passwordInp.value = user.password
+    }}, [])
+
   const handleLogin = () => {
-    const account = document.getElementById('login-account') as HTMLInputElement
-    const password = document.getElementById('login-password') as HTMLInputElement
+    const {value: account} = document.getElementById('login-account') as HTMLInputElement
+    const {value: password} = document.getElementById('login-password') as HTMLInputElement
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      body: JSON.stringify({account, password}),
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+      }
+    })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      
+      const { account: apiAccount } = res
+      if(apiAccount === account) {
+        window.localStorage.setItem('user', JSON.stringify({account, password}))
+        router.push('/dashboard')
+      }
+    })
   }
   return (
     <div className="flex items-center justify-center h-screen">
